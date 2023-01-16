0d657f84198ef1cd27f4eb1cbc05bc77d97dec9e
Nirati Khanna <niratikhanna@gmail.com>
2023-01-17 00:21:14 +0530

    Add files via upload

---
+++ Website/scripts/_DBConnect.php
@@ -0,0 +1,13 @@
+<?php
+
+
+
+//production server
+
+$servername = "remotemysql.com";
+$username = "6tuOj3AD92";
+$password = "Itgspbw6uY" ;
+$database = "6tuOj3AD92";
+$conn = mysqli_connect($servername,$username,$password,$database);
+
+?>
